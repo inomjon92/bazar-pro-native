74841e3cbf7b2f08f5e8c0ef51074fec753dd639
inomjon92 <102472300+inomjon92@users.noreply.github.com>
2026-09-17 11:50:53 +0500

    Add MainActivity with alarm timer and toast message

---
+++ app/src/main/java/com/axror/bazarcontrol/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.axror.bazarcontrol
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        QarzAlarmReceiver.startAlarmTimer(this)
+        Toast.makeText(this, "BazarControl Pro (Kotlin) faollashdi! 🔒", Toast.LENGTH_LONG).show()
+    }
+}
